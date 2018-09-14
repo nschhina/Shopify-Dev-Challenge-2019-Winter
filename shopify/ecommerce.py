8cd3cde16c5ce6d39f10f5316520ab90d1551ed5
Navraj Singh Chhina <navuchhina@live.com>
2018-09-14 00:23:48 -0400

    unncessary folder deleted

--- shopify/ecommerce.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-from .ecommerce import views.ListShoppingCartAView, views.ListShoppingCartBView
-
-router = routers.DefaultRouter()
-router.register(r'ShopA', ListShoppingCartAView)
-router.register(r'ShopB', ListShoppingCartBView)
