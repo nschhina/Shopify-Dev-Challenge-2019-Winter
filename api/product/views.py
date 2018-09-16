4ed17395a8ff8eb251567181b5b21a8edd2d33d4
Navraj Singh Chhina <navuchhina@live.com>
2018-09-15 23:08:29 -0400

    CRUD for Product Model finished.

--- api/product/views.py
+++ api/product/views.py
@@ -23,26 +23,26 @@ class ProductList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProductDetail(APIView):
-    def get_object(self, pk):
+    def get_object(self, product_name):
         try:
-            return Product.objects.get(pk=pk)
+            return Product.objects.get(product_name=product_name)
         except Product.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk):
-        snippet = self.get_object(pk)
+    def get(self, request, product_name):
+        snippet = self.get_object(product_name)
         serializer = ProductSerializer(snippet)
         return Response(serializer.data)
 
-    def put(self, request, pk):
-        snippet = self.get_object(pk)
-        serializer = ProductSerializer(snippet, data=request.data)
+    def put(self, request, product_name):
+        snippet = self.get_object(product_name)
+        serializer = ProductSerializer(snippet, data=request.data, partial = True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        snippet = self.get_object(pk)
+    def delete(self, request, product_name):
+        snippet = self.get_object(product_name)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
