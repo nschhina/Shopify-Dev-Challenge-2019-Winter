76ced6dcd645fda2d4ded6175a7df75f64ac7091
Navraj Singh Chhina <navuchhina@live.com>
2018-09-16 14:02:00 -0400

    Model for the API is finally DONE!

--- api/lineitem/models.py
+++ api/lineitem/models.py
@@ -7,5 +7,7 @@ from order.models import Order
 class LineItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    class Meta:
+        unique_together= ('order','product')
     def __str__(self):
         return self.product.product_name
